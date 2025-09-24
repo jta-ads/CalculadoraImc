b97cfd14621f945bdfd1e8336ce86b2240765fad
Jota Castro <jose.ribamar13@gmail.com>
2025-09-24 16:08:08 -0300

    Terminando a parte de lógica da calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import CalcImc from './components/CalcImc'
+import ImcTable from './components/ImcTable'
 
 function App() {
   const [imc, setImc] = useState("")
+  const [situacao, setSituacao] = useState("")
+  const [cor, setCor] = useState("")
 
   const CalculadoraImc = (e, altura, peso) =>{
     e.preventDefault();
@@ -12,15 +15,33 @@ function App() {
 
     const resultImc = (calcPeso/(calcAltura*calcAltura)).toFixed(1);
     setImc(resultImc)
+    if(resultImc < 18.5){
+      setSituacao("Magreza")
+      setCor({color:'#E8A427'})
+    } else if(resultImc >= 18.5 && resultImc <= 24.9){
+      setSituacao("Normal")
+      setCor({color:'#04B813'})
+    }else if(resultImc >= 25 && resultImc <= 29.9){
+      setSituacao("Sobrepeso")
+      setCor({color:'#EBD20C'})
+    }else if(resultImc >= 30 && resultImc <= 39.9){
+      setSituacao("Obesidade")
+      setCor({color:'#E66F10'})
+    }else{
+      setSituacao("Obesidade Grave")
+      setCor({color:'#F20A0A'})
+    }
     
   }
+  const backInicio = ()=>{
+    setImc("")
+    setSituacao("")
+    setCor("")
+  }   
   return (
     <>
       <main id='main-container'>
-        <CalcImc action={CalculadoraImc}/>
-       <div>
-        {!imc ? 'falso':'verdadeiro'}
-       </div>
+        {!imc ? <CalcImc action={CalculadoraImc}/>:<ImcTable imc={imc} situacaoAtual={situacao} estilo={cor} back={backInicio}/>}
       </main>
     </>
   )
---
+++ src/components/ImcTable.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import BtnImc from './BtnImc'
+
+const ImcTable = ({imc, situacaoAtual, estilo, back}) => {
+  return (
+    <>
+        <div id='table-container'>
+            <h1>Imc: <span style={estilo}>{imc}</span></h1>
+            <h2>Situação Atual: <span style={estilo}>{situacaoAtual}</span></h2>
+        </div>  
+        <div id="table">
+            <p>Tabela de Informações: </p>
+            <table>
+                <thead>
+                    <tr>
+                        <td>IMC</td>
+                        <td>Classificação</td>
+                        <td>Obesidade</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Menor que 18,5</td>
+                        <td>Magreza</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>Entre 18,5 e 24,9</td>
+                        <td>Normal</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>Entre 25 e 29,9</td>
+                        <td>Sobrepeso</td>
+                        <td>I</td>
+                    </tr>
+                    <tr>
+                        <td>Entre 30 e 39,9</td>
+                        <td>Obesidade</td>
+                        <td>II</td>
+                    </tr>
+                    <tr>
+                        <td>Maior que 40</td>
+                        <td>Obesidade Grave</td>
+                        <td>III</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <BtnImc id='voltar' text='Voltar' action={back}/>    
+    </>
+  )
+}
+
+export default ImcTable
