3cea38c7430b91090043025aabb69c894e86bfc2
Jota Castro <jose.ribamar13@gmail.com>
2025-09-25 11:33:46 -0300

    Estilizando a page inicial

--- src/components/BtnImc.jsx
+++ src/components/BtnImc.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./BtnImc.css"
 
 const BtnImc = ({id, text, action}) => {
     const handleAction = (e) =>{
--- src/components/CalcImc.jsx
+++ src/components/CalcImc.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import BtnImc from './BtnImc'
+import "./CalcImc.css"
 
 const CalcImc = ({action}) => {
     const [altura, setAltura] = useState("")
